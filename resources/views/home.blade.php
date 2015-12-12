e2debde4e92ff2c1fda9f41b2135e5a6d966fb3d
Admin <Admin>
2015-12-12 15:31:14 +0600

    1 commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+@section('content')
+        <!-- Main Content -->
+<div class="container">
+    <div class="row">
+        <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+            @foreach($posts as $post)
+                <div class="post-preview">
+                    <a href="post.html">
+                        <h2 class="post-title">
+                            {!! str_limit($post->title, 50) !!}
+                        </h2>
+                    </a>
+
+                    <p class="post-meta">Posted on {{ $post->created_at->format('F') }} {{ $post->created_at->format('d') }}, {{ $post->created_at->format('Y') }} at {{ $post->created_at->format('H:i:s') }}</p>
+                </div>
+                <hr>
+            @endforeach
+            {!! $posts->render()!!}
+            {{--<ul class="pager">--}}
+                {{--<li class="next">--}}
+                    {{--<a href="#">Older Posts &rarr;</a>--}}
+                {{--</li>--}}
+            {{--</ul>--}}
+        </div>
+    </div>
+</div>
+@endsection
